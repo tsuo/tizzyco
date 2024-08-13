9872ee104b600c88dab544b320aa56dd66f5cb7c
Dylan Gearhart <124414098+dmgear@users.noreply.github.com>
2024-08-12 20:24:27 -0400

    fixed project

--- Sudoku/Program.cs
+++ Sudoku/Program.cs
@@ -1,35 +1,5 @@
 ﻿using System;
 
-class Sudoku
+partial class Sudoku
 {
-    // add all classes and methods in this sudoku game class
-
-    class sudokuBoard
-    {
-        private int[,] board = new int[9, 9];
-
-        public sudokuBoard(int[,] initial = null)
-        {
-            // Initialize the board or load a puzzle
-
-            if (initial != null && initialBoard.GetLength(0) == 9 && initialBoard.GetLength(1) == 9))
-
-                board = initial;
-
-            else
-
-                board = new int[9, 9];
-        }
-
-        public int[] GetSection(int section)
-        {
-            int sectionIndex;
-
-            int x = section / 3;
-            int y = section % 3;
-
-            return [x, y];
-
-        }
-    }
 }
---
+++ Sudoku/SudokuBoard.cs
@@ -0,0 +1,33 @@
+﻿public class Sudoku
+{
+    // add all classes and methods in this sudoku game class
+
+    class SudokuBoard
+    {
+        private int[,] board = new int[9, 9];
+
+        public SudokuBoard(int[,] initial = null)
+        {
+            // Initialize the board or load a puzzle
+
+            if (initial != null && initial.GetLength(0) == 9 && initial.GetLength(1) == 9)
+
+                board = initial;
+
+            else
+
+                board = new int[9, 9];
+        }
+
+        public int[] GetSection(int section)
+        {
+            int sectionIndex;
+
+            int x = section / 3;
+            int y = section % 3;
+
+            return [x, y];
+
+        }
+    }
+}
